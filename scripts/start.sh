857e5c367663b733647de0d38c61d9e6a3b06555
Alex Shoronov <alexshoronov@gmail.com>
2016-11-17 01:08:29 +0300

    Added new hack for Heroku

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #
 # This script bind TCP-listener to port equal environment
-# variable for success start Heroku dyno.
+# variable for success start Heroku dyno. Also this script
+# make periodic requests to host for prevent dyno from moving
+# to idle state.
 #
 # Author: Alex Shoronov <alexshoronov@gmail.com>
 
 BORISYCH_SERVER_TEXT="У нас все можно"
+HEROKU_HOST='borisych.herokuapp.com'
+REQUEST_INTERVAL=1200000
 
 if [ -n $PORT ]; then
 	node -e "\
-		require('http')\
-		.createServer((req, res) => {\
+		var http = require('http');\
+		setInterval(() => {\
+			http.request({ host: '${HEROKU_HOST}' });\
+		}, ${REQUEST_INTERVAL});\
+		http.createServer((req, res) => {\
 			res.end('${BORISYCH_SERVER_TEXT}');\
 		})\
 		.listen(process.env.PORT);\
